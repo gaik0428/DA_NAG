510ce96f079d44dceee468659977f90405f280c7
gaik0428 <117895079+gaik0428@users.noreply.github.com>
2023-02-15 13:38:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import pandas as pd
-df1 = pd.read_excel("Project_File.xlsx", usecols = 'A, AE, AF, AG, AH, AI')
-df2 = df1.loc[360:480]
-df2 
+import openpyxl as pip
+
+df1 = pd.read_excel(r'C:\Users\Gaik Hwa\PycharmProjects\DA_NAG\Project_File.xlsx',  usecols = 'A, AE, AF, AG, AH, AI')
+print(df1)
+
+df2 = df1.loc[360:479]
+print(df2)
+
+print(df2.columns)
+
+df2.rename(columns = {'   ':'Date'}, inplace = True)
+print(df2)
+
+df2[['Year','Month']] = df2.Date.str.split(" ",expand = True)
+print(df2) 
