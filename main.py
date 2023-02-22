0a931573e6983a476fd1bfb3d43e3e610e16bbc3
antozesslyn <117894674+antozesslyn@users.noreply.github.com>
2023-02-22 13:10:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,40 @@
 import pandas as pd
 import openpyxl as pip
 
-# read the excel & filter the region
-df1 = pd.read_excel(r"C:\Users\LENOVO\Documents\DEG\- Applied scripting\ASP project\project_file.xlsx",  usecols = 'A, AE, AF, AG, AH, AI')
-print(df1)
+# read the excel, filter the region & filter the timeframe
+relevant_df = pd.read_excel(r"C:\Users\LENOVO\Documents\DEG\- Applied scripting\ASP project\project_file.xlsx",  usecols = 'A, AE, AF, AG, AH, AI')
+relevant_df = relevant_df.loc[360:479]
+print(relevant_df)
 
-# filter the timeframe
-df2 = df1.loc[360:479]
-print(df2)
-
-print(df2.columns)
+# to find the column names
+print(relevant_df.columns)
 
 # rename the blank column
-df3 = df2.rename(columns={'   ':'Date'})
-print(df3)
+relevant_df = relevant_df.rename(columns={'   ':'Date'})
+print(relevant_df)
 
 # split the column
-date = df3["Date"].str.split(' ', n=2, expand = True)
-df3["Year"] = date[1]
-df3["Month"] = date[2]
-print(df3.head())
+date = relevant_df["Date"].str.split(' ', n=2, expand = True)
+relevant_df["Year"] = date[1]
+relevant_df["Month"] = date[2]
+print(relevant_df.head())
 
 # remove unnecessary column
-df3.pop('Date')
-df3.pop('Month')
-print(df3)
+relevant_df.pop('Date')
+relevant_df.pop('Month')
+print(relevant_df)
 
 # change columns' datatypes
-df3[[ ' USA ', ' Canada ', ' Australia ', ' New Zealand ', ' Africa ']].apply(pd.to_numeric)
-print(df3.dtypes)
+relevant_df[[ ' USA ', ' Canada ', ' Australia ', ' New Zealand ', ' Africa ']].apply(pd.to_numeric)
+print(relevant_df.dtypes)
 
-# sum the regions over 10 yrs [ separate & total ]
-sum_df = df3.groupby('Year').sum()
-print(sum_df)
+# sum the regions over 10 yrs [ separate & total ] in a new dataframe
+summed_relevant_df = relevant_df.groupby('Year').sum()
+summed_relevant_df.loc["Sum"] = summed_relevant_df.sum(axis=0)
+print(summed_relevant_df)
+
+# Top 3 Countries in order : Australia, USA & New Zealand
+
 
 sum_df.loc["Sum"] = sum_df.sum(axis=0)
 print(sum_df)
