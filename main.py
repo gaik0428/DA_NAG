804b49223295ffce02223d98882979e789c4f562
antozesslyn <117894674+antozesslyn@users.noreply.github.com>
2023-02-22 14:49:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import openpyxl as pip
+import matplotlib.pyplot as pls
 
 # read the excel, filter the region & filter the timeframe
 relevant_df = pd.read_excel(r"C:\Users\LENOVO\Documents\DEG\- Applied scripting\ASP project\project_file.xlsx",  usecols = 'A, AE, AF, AG, AH, AI')
@@ -28,10 +29,18 @@ print(relevant_df)
 relevant_df[[ ' USA ', ' Canada ', ' Australia ', ' New Zealand ', ' Africa ']].apply(pd.to_numeric)
 print(relevant_df.dtypes)
 
-# sum the regions over 10 yrs [ separate & total ] in a new dataframe
-summed_relevant_df = relevant_df.groupby('Year').sum()
-summed_relevant_df.loc["Sum"] = summed_relevant_df.sum(axis=0)
-print(summed_relevant_df)
+# sum the regions over 10 yrs [ separate ] in a new dataframe
+yr_summed_df = relevant_df.groupby('Year').sum()
+yr_summed_df.loc["Sum"] = yr_summed_df.sum(axis=0)
+print(yr_summed_df)
+
+# Only the total sum for visualization
+total_summed_df = yr_summed_df.iloc[[10]]
+print(total_summed_df)
+
+# visualization for top 3 countries over 10 yrs
+Top_countries_bc = total_summed_df.plot(kind="bar", title = 'International Monthly Visitors for 2008 to 2017', stacked = False ,figsize=(10,10), legend =True,fontsize=12)
+pls.show()
 
 # Top 3 Countries in order : Australia, USA & New Zealand
 
