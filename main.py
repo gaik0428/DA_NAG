8c778bb5256c11b1c0e5e79215cbe59e7538475a
antozesslyn <117894674+antozesslyn@users.noreply.github.com>
2023-02-22 11:24:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import openpyxl as pip
 df1 = pd.read_excel(r"C:\Users\LENOVO\Documents\DEG\- Applied scripting\ASP project\project_file.xlsx",  usecols = 'A, AE, AF, AG, AH, AI')
 print(df1)
 
-# filter the region
+# filter the timeframe
 df2 = df1.loc[360:479]
 print(df2)
 
@@ -30,6 +30,11 @@ print(df3)
 df3[[ ' USA ', ' Canada ', ' Australia ', ' New Zealand ', ' Africa ']].apply(pd.to_numeric)
 print(df3.dtypes)
 
-# sum the regions over 10 yrs
+# sum the regions over 10 yrs [ separate & total ]
 sum_df = df3.groupby('Year').sum()
 print(sum_df)
+
+sum_df.loc["Sum"] = sum_df.sum(axis=0)
+print(sum_df)
+
+# Top 3 Countries in order : Australia, USA & New Zealand
