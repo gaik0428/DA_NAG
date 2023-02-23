26afcfed4709ad51d7014e46e48015660172356d
gaik0428 <tghwa2005@gmail.com>
2023-02-23 13:28:12 +0800

    Update

--- try.py
+++ try.py
@@ -1,38 +1,45 @@
 import pandas as pd
-import openpyxl as pip
-import matplotlib.pyplot as pls
+import matplotlib.pyplot as plt
+import numpy as np
 
-# the regions
-others = [[' USA ', ' Canada ', ' Australia ', ' New Zealand ', ' Africa ']]
-
-# read the excel, filter the region & filter the timeframe
+# read excel
 whole_df = pd.read_excel(r"C:\Users\Gaik Hwa\PycharmProjects\DA_NAG\Project_File.xlsx")
 
-# rename the blank column
-whole_df = whole_df.rename(columns={'   ': 'Date'})
+# rename blank column
+whole_df = whole_df.rename(columns={'   ':'Date'})
 
-# split the column
-date = whole_df["Date"].str.split(' ', n=2, expand=True)
+# split Date column to make life easier
+date = whole_df["Date"].str.split(' ', n=2, expand = True)
 whole_df["Year"] = date[1]
 whole_df["Month"] = date[2]
 print(whole_df.head())
 
-print(whole_df.columns)
-
-# Filter region & timeframe
+# filter the regions & timeframe
 whole_df = whole_df.loc[(whole_df['Year'] >= '2008')
                         & (whole_df['Year'] <= '2017')]
+whole_df = whole_df[[ ' USA ', ' Canada ', ' Australia ', ' New Zealand ', ' Africa ', 'Year']]
 relevant_df = whole_df
 print(relevant_df)
 
-# remove unnecessary column
-relevant_df.pop("Date")
-relevant_df.pop("Month")
-print(relevant_df)
-
-# change columns' datatypes
-relevant_df[[' USA ', ' Canada ', ' Australia ', ' New Zealand ', ' Africa ']].apply(pd.to_numeric)
+# change datatypes
+relevant_df = relevant_df.astype(int)
 print(relevant_df.dtypes)
 
-country_df = relevant_df("others")
-print(country_df)
+# find the total over 10 yrs
+relevant_df.pop('Year')
+total_summed_df = relevant_df.sum().sort_values(ascending=False)
+print(total_summed_df)
+
+# Top 3 countries
+print(total_summed_df.head(3))
+
+# Visualization
+array = total_summed_df.to_numpy()
+print(array)
+
+fig = plt.figure(facecolor = 'white', figsize = (10,10))
+country = np.array([ ' USA ', ' Canada ', ' Australia ', ' New Zealand ', ' Africa '])
+print(country)
+
+plt.bar(country, array)
+plt.show()
