a1f62a17d5ae3c1740cfb61f04eac36b361d2bbc
gaik0428 <117895079+gaik0428@users.noreply.github.com>
2023-02-23 13:27:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import openpyxl as pip
 import matplotlib.pyplot as plt
 
 # read excel
-whole_df = pd.read_excel(r"C:\Users\LENOVO\Documents\DEG\- Applied scripting\ASP project\project_file.xlsx")
+whole_df = pd.read_excel(r"C:\Users\Gaik Hwa\PycharmProjects\DA_NAG\Project_File.xlsx")
 
 # rename blank column
 whole_df = whole_df.rename(columns={'   ':'Date'})
